d1af1fc3287b3e5c1f0a720a25f50b625efebf5b
manikandanraj <manikandan60167@gmail.com>
2026-02-26 23:09:46 +0530

    add new code to fine minimum and maximum value fr. given array

--- sortings/Sortings.java
+++ sortings/Sortings.java
@@ -4,11 +4,12 @@ import java.util.Arrays;
 
 public class Sortings{
     public static void main(String[] args) {
-        int []array={1,-2,-3,4,5,33,0,44,1,-8,-9,55};
+        int []array={1,3,3,-9,-6,-11,57,0,44};
         System.out.println("Before sorting = "+Arrays.toString(array));
         Sortings.ascendingOrder(array);
         Sortings.descendingOrder(array);
         Sortings.moveNegagive(array);
+        Sortings.minMaxValue(array);
     }
 
     public static void ascendingOrder(int []arr){
@@ -56,6 +57,21 @@ for(int i=0;i<arr.length;i++){
     }
 }
 System.out.println("Move all negative elements in starting posisition (after): "+Arrays.toString(a));
+}
+
+// find the minimum and maximum value from given array
+public static void minMaxValue(int[]arr){
+int min=Integer.MAX_VALUE,max=Integer.MIN_VALUE;
+for(int i=0;i<arr.length;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+        else if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    System.out.println("Min Value = "+min);
+System.out.println("Max Value = "+max);
 }
 
 }
