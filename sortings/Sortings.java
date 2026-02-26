0e7dac0ce7731e4062a096f6bbfffaccf3d9f7b7
manikandanraj <manikandan60167@gmail.com>
2026-02-26 22:40:40 +0530

    add new practiced code which is perform move all negative elements in starting position....

--- sortings/Sortings.java
+++ sortings/Sortings.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 
 public class Sortings{
     public static void main(String[] args) {
-        int []array={1,-2,-3,4,5,33,44,1,-8,-9,55};
+        int []array={1,-2,-3,4,5,33,0,44,1,-8,-9,55};
         System.out.println("Before sorting = "+Arrays.toString(array));
         Sortings.ascendingOrder(array);
         Sortings.descendingOrder(array);
+        Sortings.moveNegagive(array);
     }
 
     public static void ascendingOrder(int []arr){
@@ -38,4 +39,23 @@ public class Sortings{
         }
         System.out.println("Descending Order"+Arrays.toString(arr));
     }
+        // Move all negative elements in starting
+public static void moveNegagive(int []arr){
+int index=0;
+System.out.println("befor: "+Arrays.toString(arr));
+int []a=new int[arr.length];
+for(int i=0;i<arr.length;i++){
+    if(arr[i]<0){
+        a[index]=arr[i];
+        index+=1;
+    }}
+    for(int i=0;i<arr.length;i++){
+    if(arr[i]>=0){
+        a[index]=arr[i];
+        index+=1;
+    }
+}
+System.out.println("Move all negative elements in starting posisition (after): "+Arrays.toString(a));
+}
+
 }
