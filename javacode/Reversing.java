f48441ab6191a97a5b4b0813a668fc6d40155eab
manikandanraj73 <manikandan60167@gmail.com>
2026-03-15 23:16:46 +0530

    Update Reversing.java

--- javacode/Reversing.java
+++ javacode/Reversing.java
@@ -94,7 +94,7 @@ public static void checkPalindrom(){
         int [] arr={1,1,1,2,3,0,0,44,0,55,3,5};
         int[] removeDup=new int[arr.length];
         int k=0;
-        //  
+      
         for(int i=0;i<arr.length;i++){
             boolean isExist=false;
             for(int j=0;j<k;j++){
