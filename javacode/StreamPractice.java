36792c12a051e6d4a8ca91e596c971609ac8db84
manikandanraj <manikandan60167@gmail.com>
2026-03-25 21:44:33 +0530

    start the new level like solve the problem using Stream()

--- javacode/StreamPractice.java
+++ javacode/StreamPractice.java
@@ -1,1 +1,19 @@
+import java.text.Collator;
+import java.util.Arrays;
+import java.util.List;
+// import java.util.Optional;
+// import java.util.function.Function;
+// import java.util.function.Predicate;
+// import java.util.stream.Stream;
 
+public class StreamPractice{
+public static void main(String[] args) {
+  List<Integer>list=Arrays.asList(1,10,30,15,2,9);
+ int res=list.stream()
+ .filter(n->n%2==0)
+
+  .reduce(0,(n,a)->n+a);
+  
+  System.out.println(res);
+}
+}
