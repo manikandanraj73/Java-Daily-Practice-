570d7ca5a7e422eac598b8c2660db34e2732a353
manikandanraj <manikandan60167@gmail.com>
2026-03-28 22:06:41 +0530

    add new code for find the max and min and etc....

--- javacode/StreamPractice.java
+++ javacode/StreamPractice.java
@@ -1,19 +1,50 @@
-import java.text.Collator;
-import java.util.Arrays;
 import java.util.List;
-// import java.util.Optional;
-// import java.util.function.Function;
-// import java.util.function.Predicate;
-// import java.util.stream.Stream;
-
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.Arrays;
 public class StreamPractice{
 public static void main(String[] args) {
-  List<Integer>list=Arrays.asList(1,10,30,15,2,9);
- int res=list.stream()
- .filter(n->n%2==0)
+List<Integer>list=Arrays.asList(2,3,4,4,56,1,7,22);
+StreamPractice.findFactorial();
+}
+// find the minimum and maximum value of that given list
+public static void findminMaxValues(List<Integer> list){
+int max=list.stream().mapToInt(n->n).max().orElse(0);
+int min=list.stream().mapToInt(n->n).min().orElse(0);
+System.out.println("Max value is this List : "+max );
+System.out.println("Min value is this List : "+min );
+}
+// find the Odd or Even numbers in the List
+public static void showEvenNumbers(List<Integer> list){
+    System.out.println("Even Numbers");
+    list.stream().filter(n->n%2==0).forEach(n->System.out.print(n+", "));
+    System.out.println("\nOdd Numbers");
+    list.stream().filter(n->n%2==1).forEach(n->System.out.print(n+", "));
+}
+public static void findFrequencyofCharacter(){
+    String str="aabbbc";
+Map<Character,Long>frequency=str.chars()
+    .mapToObj(c->(char)c)
+    .collect(Collectors.groupingBy(c->c,
+    Collectors.counting()));
 
-  .reduce(0,(n,a)->n+a);
-  
-  System.out.println(res);
+    System.out.println(frequency);
+}
+public static void findFrequencyofNums(List<Integer> list){
+    Map<Integer,Long> freq=list.stream()
+    .collect(Collectors.groupingBy(n->n,
+    Collectors.counting()));
+    System.out.println(freq);
+}
+public static void findFactorial()
+{
+    int a=3;
+    int factorial=IntStream.rangeClosed(1, a)
+    .reduce(1, (x,y)-> x*y);
+    System.out.println(a+"'s "+factorial);
 }
 }
