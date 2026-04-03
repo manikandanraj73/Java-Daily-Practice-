1af1555e1634f9888529cd2bd9cd2071a79abeae
manikandanraj <manikandan60167@gmail.com>
2026-04-03 20:17:35 +0530

    add a new prcticed patterns which is hollowDiamondPattern and hollowTrianglePattern()

--- Patterns/Patterns.java
+++ Patterns/Patterns.java
@@ -2,10 +2,49 @@ package Patterns;
 
 public class Patterns{
     public static void main(String[] args) {
-    Patterns.checkBoardPattern();
+    Patterns.hollowDiamondPattern();
     
     }
-    public static void hollowPattern(){
+    private static void hollowDiamondPattern() {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int space=5;space>i;space--){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i*2-1;j++){
+                if(j==1||j==i*2-1)
+                System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>=1;i--){
+            for(int space=5;space>i;space--){
+                System.out.print(" ");
+            }
+            
+            for(int j=1;j<=i*2-1;j++){
+            if(j==1||j==i*2-1)
+                System.out.print("*");
+            
+            else System.out.print(" ");
+        }
+            System.out.println();
+        }
+    }
+    private static void hollowTrianglePattern() {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if(j==1||j==i||i==n){
+                    System.out.print("* ");
+                }
+                else{System.out.print("  ");}
+            }
+        System.out.println();
+        }
+    }
+    public static void hollowBoxPattern(){
         int n=5;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
